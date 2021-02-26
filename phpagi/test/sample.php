96f342de2c8ae3cb4edebca99df5a59e921ec719
Jorge Santiago <georgeosantiago@yahoo.com>
2021-02-26 14:38:05 -0300

    Limpieza

--- phpagi/test/sample.php
+++ phpagi/test/sample.php
@@ -1,4 +1,21 @@
 <?php
+
+/*
+[from-internal-custom]	
+
+exten => 351,1,Noop(Extension 351)
+same => n,Answer
+same => n,Wait(1)
+same => n,Set(campo1=valor1)
+same => n,Playback(custom/saludo)
+same => n,AGI(agi://192.168.0.52/test/sample.php,ejemplo1)
+
+same => n,NoOp(campo1: ${campo1})
+same => n,NoOp(test: ${test})
+
+same => n,Hangup
+*/
+
 	// Parametros del PHP FastAGI bootstrap
 	$request = $fastagi->request;
 		
